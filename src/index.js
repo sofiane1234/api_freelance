0bb4da75f49291ca0aa56026247d91b884afbb49
unknown <sofiane.draris@ynov.com>
2023-01-03 13:01:41 +0100

    added  listener

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('Sosso')
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const app = express();
+
+app.listen(process.env.PORT, function () {
+    console.log('Demarrage du server sur le port : ' + process.env.PORT)
+});
